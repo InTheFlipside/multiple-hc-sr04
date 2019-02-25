0a50e9078607da7ed9434f2147133c7605542150
idkhbtfm <26010778+InTheFlipside@users.noreply.github.com>
2019-02-25 14:29:47 +0100

    Create Program for 4 + antibug.ino

---
+++ Program for 4 + antibug.ino
@@ -0,0 +1,150 @@
+// Capteur 1
+const byte TRIGGER_PIN = 2; // Broche TRIGGER
+const byte ECHO_PIN = 3;    // Broche ECHO
+// Capteur 2
+const byte TRIGGER_PIN2 = 4; // Broche TRIGGER
+const byte ECHO_PIN2 = 5;    // Broche ECHO
+// Capteur 3
+const byte TRIGGER_PIN3 = 6; // Broche TRIGGER
+const byte ECHO_PIN3 = 7;    // Broche ECHO
+// Capteur 4
+const byte TRIGGER_PIN4 = 8; // Broche TRIGGER
+const byte ECHO_PIN4 = 9;    // Broche ECHO
+
+const byte timetostart = 10;
+
+/// définition des constantes. Pas besoin de définir ça pour chacun (le timeout / vitesse du son ne change pas) 
+const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = ~8m à 340m/s
+const float SOUND_SPEED = 340.0 / 1000;
+
+
+const byte DEL = 11;
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(DEL,OUTPUT)
+   // on indique quelle pin sert à quoi pour chaque capteur
+  pinMode(TRIGGER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, LOW); 
+  pinMode(ECHO_PIN, INPUT);
+
+  pinMode(TRIGGER_PIN2, OUTPUT);
+  digitalWrite(TRIGGER_PIN2, LOW); 
+  pinMode(ECHO_PIN2, INPUT);
+
+    pinMode(TRIGGER_PIN3, OUTPUT);
+  digitalWrite(TRIGGER_PIN3, LOW); 
+  pinMode(ECHO_PIN3, INPUT);
+
+    pinMode(TRIGGER_PIN4, OUTPUT);
+  digitalWrite(TRIGGER_PIN4, LOW);
+  pinMode(ECHO_PIN4, INPUT);
+  
+}
+ 
+void loop() {
+
+
+
+  ////////////// MESURE CAPTEUR 1 //////////
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+
+
+  long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+  float distance_mm = measure / 2.0 * SOUND_SPEED;
+  
+  Serial.print(F("Distance Capteur 1 : "));
+  Serial.print(distance_mm);
+  Serial.print (F("mm (")); // pas de "println" comme ça on met tout les capteurs sur la même ligne
+
+
+
+  /////////////MESURE CAPTEUR 2 /////// 
+
+ 
+  digitalWrite(TRIGGER_PIN2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN2, LOW);
+  
+  long measure2 = pulseIn(ECHO_PIN2, HIGH, MEASURE_TIMEOUT); //// Normalement, une fois que la mesure du capteur 1 est bonne, on devrait pouvoir réutiliser la variable "measure" et "distance_mm". mais c'est plus facile d'en faire une autre. puis ça aidera au debuggage
+  float distance_mm2 = measure2 / 2.0 * SOUND_SPEED; 
+
+  Serial.print(F("                Distance Capteur 2 : "));
+  Serial.print(distance_mm2);
+  Serial.print(F("mm (")); // Toujours pas de println. 
+
+
+/////////////////MESURE CAPTEUR 3 /////////
+
+
+  digitalWrite(TRIGGER_PIN3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN3, LOW);
+  
+  long measure3 = pulseIn(ECHO_PIN3, HIGH, MEASURE_TIMEOUT);
+  float distance_mm3 = measure3 / 2.0 * SOUND_SPEED; 
+
+  Serial.print(F("                Distance Capteur 3 : "));
+  Serial.print(distance_mm3);
+  Serial.print(F("mm (")); 
+
+
+/////////////////MESURE CAPTEUR 4 /////////
+
+
+  digitalWrite(TRIGGER_PIN4, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN4, LOW);
+  
+  long measure4 = pulseIn(ECHO_PIN4, HIGH, MEASURE_TIMEOUT);
+  float distance_mm4 = measure4 / 2.0 * SOUND_SPEED; 
+
+  Serial.print(F("                Distance Capteur 4 : "));
+  Serial.print(distance_mm4);
+  Serial.println(F("mm ("));  /// et là on met enfin le println
+
+
+
+
+if( distance_mm < 51) /// CAPTEUR 1 à -50mm)
+{
+/// ON REFAIT LA MESURE ///
+
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+
+  long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+  float distance_mm = measure / 2.0 * SOUND_SPEED;
+/// MESURE FINIE ///
+
+// on re-analyse la distance pour vérifier//
+
+if (distance_mm < 51) {
+
+  Serial.println("VERIFICATION : CHECK");
+  analogWrite(DEL,50);
+  delay(100);
+  analogWrite(DEL,100);
+  delay(100);
+  analogWrite(DEL,150);
+  delay(100);
+  analogWrite(DEL,200);
+  delay(100);
+  analogWrite(DEL,255);
+  delay(100);
+}
+
+ 
+  
+}
+
+
+  delay(timetostart); // voir "timetostart" au début. permet de définir le nb de milliseconde avant que le programme se relance
+  
+
+  
+}
